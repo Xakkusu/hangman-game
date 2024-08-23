78c24ce6d083908af51fde1d067467a494355f9d
Naomi Dax <imkadax@gmail.com>
2024-08-23 16:10:10 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def game_menu():
+
+def get_game_instructions():
+
+def go_to_main_menu():
+
+def choose_game_category():
+
+def start_game():
+
+def get_hangman_data(data):
+
+def try_gain():
+
+def won_round():
+
+def end_game()
+
+def main ():
