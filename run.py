8a1ffbd864bbcef6b59ae31e0e2d48d5ec0dd351
Naomi Dax <imkadax@gmail.com>
2024-08-26 08:57:17 +0000

    feat: Add game_menu function to display menu at the start of the game

--- run.py
+++ run.py
@@ -3,21 +3,34 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def game_menu():
+    """
+    Function to display when first running program with the menu.
+    Menu has options to either start the game, show instrucions, show leaderboard or exit from program.
+    """
+    print("Hang the Man!")
+    print("However you ended here really doesn't matter does it, it only matters what you want to do now\n")
+    print("Read through the menu and chose one option")
+    print(f"1. Start the game!\n2. How to play?\n3. Leaderboard\n1. I am done!\n")
+    user_choice = input("What is your choice: ")
+    print(user_choice)
+
 
 def get_game_instructions():
-
+    """"""
 def go_to_main_menu():
-
+    """"""
 def choose_game_category():
-
+    """"""
 def start_game():
-
+    """"""
 def get_hangman_data(data):
-
+    """"""
 def try_gain():
-
+    """"""
 def won_round():
-
-def end_game()
-
+    """"""
+def end_game():
+    """"""
 def main ():
+    """"""
+game_menu()
