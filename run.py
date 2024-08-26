e52c32fc0fb050b60985730ecaebf447312be4e7
Naomi Dax <imkadax@gmail.com>
2024-08-26 13:06:31 +0000

    feat: Add instructions function with possibility to get to the menu or exit the programme

--- run.py
+++ run.py
@@ -15,6 +15,8 @@ def game_menu():
     user_choice = input("What is your choice: ")
     print(user_choice)
     validate_menu_choice(user_choice)
+    if int(user_choice) == 2:
+       get_game_instructions() 
 
 
 def validate_menu_choice(data):
@@ -25,19 +27,61 @@ def validate_menu_choice(data):
     integer or is not between 1 and 4
     """
     try:
-        int(data)
-        if data<1 or data>4:
+        nmbr = int(data)
+        if nmbr<=0 or nmbr>=5:
             raise ValueError(
-                f"You entered {data}"
+                f"You entered {nmbr}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\nRemember your choice needs to be a number between 1 and 4")
-    finally:
-        print("You will be redirected to the start of the menu...\n")
+        print(f"Invalid data: {e}, please try again.\nRemember your choice needs to be a number between 1 and 4.\nYou will be redirected to the start of the menu...\n")
+        game_menu()
 
 
 def get_game_instructions():
-    """"""
+    """
+    Instructions are displayed as well as the option to 
+    return to the main menu
+    """
+    instructions = """
+       ____________________________________________
+     / \                                            \.
+    |   |               How to play                 |.
+     \_ |                                           |.
+        |  Each blank line stands for a character   |.
+        |  in a secret word.                        |.    
+        |                                           |.
+        |  Guess the letters for each line ony by   |.
+        |  one.                                     |.
+        |                                           |.
+        |  If correct the according blank space     |.
+        |  will be filled in with the letter.       |.
+        |                                           |.
+        |  You have 7 tries.                        |.
+        |                                           |.
+        |  With each mistake the stick figure of    |.
+        |  a person being hung will be drawn until  |.
+        |  the drawing of the hanged man is         |.
+        |  completed & the player lost.             |.
+        |                                           |.
+        |  Try your best & save the man, or not...  |.    
+        |                                           |.            
+        |   ________________________________________|___
+        |  /                                            /.
+        \_/____________________________________________/.\n"""
+    print(instructions)
+    print("To go back to the menu enter 'menu'.\nTo exit enter 'exit'\n")
+    instruction_choice_user = input("Enter here: ")
+    while True:
+        if instruction_choice_user.lower() == "menu":
+            game_menu()
+            break
+        elif instruction_choice_user.lower() == "exit":
+            #will be added once function is written
+            break
+        else:
+            print("Look who the cat dragged in... Once again to go back to the menu enter 'menu'.\nTo exit enter 'exit'\n")
+            instruction_choice_user = input("Enter here: ")
+
 def go_to_main_menu():
     """"""
 def choose_game_category():
